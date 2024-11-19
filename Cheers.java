22170bec2b7766f486982ab835603d031aed22b0
unknown <itamar12332@gmail.com>
2024-11-19 22:41:14 +0200

    complted all tasks

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,61 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String chears = args[0];       
+        int reapet = Integer.parseInt(args[1]);
+        int iteration = chears.length();
+        System.out.println(chears.charAt(0));
+        for(int i= 0; i < iteration ; i++)
+        {
+        switch  (chears.charAt(i)) {
+                case 'A':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'E':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'F':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'H':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'I':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'L':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                         break;
+                case 'M':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'N':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;        
+                case 'O':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+
+                case 'R':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'S':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                case 'X':
+                        System.out.println("Give me an "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
+                        break;
+                default:
+                        System.out.println("Give me a  "+chears.charAt(i)+":" +" "+chears.charAt(i)+"!");
         }
+        }
+        System.out.println("What does that spell?");
+        for(int i =0 ; i<reapet;i++){
+                System.out.println(chears+"!!!");
+        }          
+
+        }        
+        
+
+        
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,40 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int seedN = Integer.parseInt(args[0]);
+		String command = args[1];
+		
+		int seed_min = 1;
+		int tempNumber = 0;
+
+		for (int i = 0; i < seedN; i++) {
+			
+			tempNumber = seed_min;
+			int counter_steps = 1;
+
+			while (tempNumber != 1 || counter_steps == 1) {
+				if (tempNumber % 2 == 0) {
+					if (command.equals("v")){
+					System.out.print(tempNumber + " ");
+					}
+					tempNumber /= 2;
+					counter_steps++;
+				} else {
+					if (command.equals("v")){
+					System.out.print(tempNumber + " ");
+					}
+					tempNumber = (tempNumber * 3) + 1;
+					counter_steps++;
+				}
+			}
+			seed_min += 1;
+			if (command.equals("v")){
+			System.out.println(1 + " (" + counter_steps + ")");
+			}
+		}
+		if (tempNumber == 1) {
+			System.out.println("Every one of the first " + (seed_min - 1) + " hailstone sequences reached 1.");
+		} else {
+			System.out.println("ERROR");
+		}
 	}
 }
